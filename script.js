00845115c2d419f6368ad2b3623bd8cd6125ae50
Vishalini-2003 <86412221+Vishalini-2003@users.noreply.github.com>
2021-06-24 18:38:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var p = document.getElementById("principal").value;
+    var rate=document.getelementbyId("rate".value;
+    var years = doument.getElementsByID ("years").value;
+    var interest= principal*years*rate/100;
+    var year=new Date().getFullYear()+parsent(years);
     
 }
-        
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
